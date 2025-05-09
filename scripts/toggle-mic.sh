c92c8afcebcb58e6a0a5c931b094dfe625df9757
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-05-08 21:30:00 -0300

    refactor: remove unused echos & store toggle mic function in variable

--- scripts/toggle-mic.sh
+++ scripts/toggle-mic.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 IS_MUTED=`pactl get-source-mute @DEFAULT_SOURCE@ | grep yes`
+TOGGLE_MUTE=`pactl set-source-mute @DEFAULT_SOURCE@ toggle`
 
 if [[ -n $IS_MUTED ]]; then
-  echo "is muted"
-  pactl set-source-mute @DEFAULT_SOURCE@ toggle && paplay ~/.config/hypr/audios/discord-unmute-sound.mp3
+  $TOGGLE_MUTE && paplay ~/.config/hypr/audios/discord-unmute-sound.mp3
 else
-  echo "isn't muted"
-  pactl set-source-mute @DEFAULT_SOURCE@ toggle && paplay ~/.config/hypr/audios/discord-mute-sound.mp3
+  $TOGGLE_MUTE && paplay ~/.config/hypr/audios/discord-mute-sound.mp3
 fi
 
